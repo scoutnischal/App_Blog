c8d2536648d74c6a4a622b4b39bfae415ab1de24
scoutnischal <scoutnischal@gmail.com>
2023-02-27 15:55:07 +0545

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './components/Home';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <div>
+     <Home/>
+     <Body/>
+    </div>
+  );
+}
+
+export default App;
